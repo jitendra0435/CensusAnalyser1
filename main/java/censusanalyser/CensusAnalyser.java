220e5d18f05c129445d0fee0c07fcd6faf5783ed
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-10 19:04:32 +0530

    Refactor code to handle DRY principle violation sort

--- main/java/censusanalyser/CensusAnalyser.java
+++ main/java/censusanalyser/CensusAnalyser.java
@@ -69,7 +69,7 @@ public class CensusAnalyser {
             throw new CensusAnalyserException("No census data", CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
         }
         Comparator<IndiaCensusDAO> censusComparator = Comparator.comparing(census -> census.population);
-        this.sortDescending(censusComparator);
+        this.sort(censusComparator);
         String sortedStateCensusData = new Gson().toJson(censusList);
         return sortedStateCensusData;
 
@@ -86,20 +86,5 @@ public class CensusAnalyser {
                 }
             }
         }
-
-    }
-
-    private void sortDescending(Comparator<IndiaCensusDAO>censusComparator){
-        for(int i=0;i<censusList.size()-1;i++){
-            for(int j=0;j<censusList.size()-i-1;j++){
-                IndiaCensusDAO census1=censusList.get(j);
-                IndiaCensusDAO census2=censusList.get(j+1);
-                if(censusComparator.compare(census1,census2)<0){
-                    censusList.set(j,census2);
-                    censusList.set(j+1,census1);
-                }
-            }
-        }
-
     }
 }
