66e4b8b07719d15a293fdc3aff236721650c4b40
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-13 10:55:51 +0530

    Added:TestCode for indiaCensusAdapter

--- test/java/censusanalyser/IndiaCensusAdapterTest.java
+++ test/java/censusanalyser/IndiaCensusAdapterTest.java
@@ -4,5 +4,17 @@ import org.junit.Test;
 import java.util.Map;
 
 public class IndiaCensusAdapterTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 
+    @Test
+    public void givenIndianCensusCSVFile_ReturnCorrectRecords() {
+        Map<String, CensusDAO> numOfRecords = null;
+        try {
+            IndianCensusAdapter indianCensusAdapter = new IndianCensusAdapter();
+            numOfRecords = indianCensusAdapter.loadCensusData(IndiaCensusCSV.class, INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29, numOfRecords.size());
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
 }
